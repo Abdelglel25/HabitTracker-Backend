8fcd227a2f6985b81e1dfccb4990dbfede719119
Abdelglel Ahmed <120330067+Abdelglel25@users.noreply.github.com>
2024-05-18 04:40:56 +0300

    Initial commit

---
+++ src/test/java/com/sa/habittrackerbackend/HabitTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa.habittrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
