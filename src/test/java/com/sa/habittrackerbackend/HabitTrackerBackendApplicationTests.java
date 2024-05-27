e7fc025ae7ad03d69fae1d6c8738bcef7779c9b3
Abdelglel Ahmed <120330067+Abdelglel25@users.noreply.github.com>
2024-05-27 22:54:10 +0300

    Update Qodana Scan command in GitHub Actions
    Modified how Qodana Scan is executed in our GitHub Actions workflow. Instead of using the 'with' clause with 'args' and 'pr-mode', the workflow now executes the 'qodana scan' command directly. This change aims to streamline the process and enhance code clarity.

--- src/test/java/com/sa/habittrackerbackend/HabitTrackerBackendApplicationTests.java
+++ src/test/java/com/sa/habittrackerbackend/HabitTrackerBackendApplicationTests.java
@@ -1,13 +1,12 @@
-//package com.sa.habittrackerbackend;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class HabitTrackerBackendApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.sa.habittrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class HabitTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
